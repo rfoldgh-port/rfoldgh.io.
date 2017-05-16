96d60931313ffd9b34adc9eb9943d695879941a3
Jon Holmberg <holmberg18@gmail.com>
2017-05-15 23:36:09 -0700

    update portfolio images

--- js/scrollview.js
+++ js/scrollview.js
@@ -33,34 +33,56 @@ function onViewport(el, elClass, offset, callback) {
 
 var inView1 = false;
 var inView2 = false;
+var inView3 = false;
+var inView4 = false;
 
 function animateLeft(element){
-  if(!inView1){
+
     $(element).animate({
       opacity: 1,
       left:"+=200px",
     }, 800);
-    inView1 = true;
+
     }
-}
+
 
 function animateRight(element){
-  if(!inView2){
+
     $(element).animate({
       opacity: 1,
       right:"+=200px",
     }, 800);
-    inView2 = true;
-  }
+
+
 }
 
 
 
 
-onViewport("#portfolio", function() {
+onViewport("#ski-lift-app-desktop-id","active", 750, function() {
+  if(!inView1){
    animateLeft("#ski-lift-app-desktop-id");
+ }
+ inView1 = true;
 });
 
-onViewport("#lego-builder-app-desktop-id", function() {
+onViewport("#lego-builder-app-desktop-id","active", 750, function() {
+  if(!inView2){
    animateRight("#lego-builder-app-desktop-id");
+ }
+ inView2 = true;
+});
+
+onViewport("#fortessa-app-desktop-id","active", 750, function() {
+  if(!inView3){
+   animateLeft("#fortessa-app-desktop-id");
+ }
+ inView3 = true;
+});
+
+onViewport("#pestman-app-desktop-id","active", 750, function() {
+  if(!inView4){
+   animateRight("#pestman-app-desktop-id");
+ }
+ inView4 = true;
 });
